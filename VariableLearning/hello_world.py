e9d49c03e90a4f535d4744b24bfeb2779c06b6c0
yang.liu12 <76164587@qq.com>
2019-12-22 22:02:59 +0800

    变量的初始学习

--- VariableLearning/hello_world.py
+++ VariableLearning/hello_world.py
@@ -2,3 +2,13 @@ print("hello world")
 
 message = "hello world"
 print(message)
+
+name = "add lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)

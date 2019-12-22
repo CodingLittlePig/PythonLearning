08e5aea5fba415ad860363b304aea1b0890c35b8
yang.liu12 <76164587@qq.com>
2019-12-22 22:32:42 +0800

    列表学习

---
+++ ListLearning/ListLearning.py
@@ -0,0 +1,16 @@
+bicycles = ['trek', 'cannodale', 'redline', 'specialized']
+print(bicycles)
+print(bicycles[-1].title())
+message = 'My first bicycle was a ' + bicycles[0].title() + '.'
+print(message)
+bicycles[0] = 'honda'
+print(bicycles)
+
+bicycles.append('ducati')
+print(bicycles)
+
+bicycles.insert(0, 'ducati')
+print(bicycles)
+
+del bicycles[0]
+print(bicycles)
--- VariableLearning/hello_world.py
+++ VariableLearning/hello_world.py
@@ -12,3 +12,13 @@ first_name = "ada"
 last_name = "lovelace"
 full_name = first_name + " " + last_name
 print(full_name)
+
+print("\tPython")
+print("\nPython")
+
+favorite_language = "Python "
+print(favorite_language)
+favorite_language.rstrip()
+print(favorite_language.rstrip())
+
+print(3**3)
